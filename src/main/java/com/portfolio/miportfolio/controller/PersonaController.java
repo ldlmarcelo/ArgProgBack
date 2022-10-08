fa9cd4027125da6f86b4774036b9cfe1d6a97a41
ldlmarcelo <ldl_marcelo@hotmail.com>
2022-10-08 18:51:54 -0300

    fix CORS ERROR

--- src/main/java/com/portfolio/miportfolio/controller/PersonaController.java
+++ src/main/java/com/portfolio/miportfolio/controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://argprogfront1.web.app")
 public class PersonaController {
 
     @Autowired
