eb4e8ca182eda9bc02993d6fb33931dd525c9cba
ldlmarcelo <ldl_marcelo@hotmail.com>
2023-01-24 20:16:32 -0300

    agrego cors a auth

--- src/main/java/com/portfolio/miportfolio/security/MainSecurity.java
+++ src/main/java/com/portfolio/miportfolio/security/MainSecurity.java
@@ -18,9 +18,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
 @EnableWebSecurity
+@CrossOrigin(origins = "https://argprogfront1.web.app/")
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MainSecurity extends WebSecurityConfigurerAdapter{
     @Autowired
